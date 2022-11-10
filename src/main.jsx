efebb877d6c731a939d7eb9ed8bd314b91edadb0
jhonnymosquera <78888142+jhonnymosquera@users.noreply.github.com>
2022-11-10 08:27:56 -0500

    Routes
    Solucion del problema de las rutas al recargar la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import ContextProvider from './Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ContextProvider>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</ContextProvider>
 	</React.StrictMode>
 );
